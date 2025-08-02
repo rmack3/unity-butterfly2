3e53907084f96786861d7d6680c61667339779f9
rmack3 <68609444+rmack3@users.noreply.github.com>
2025-08-02 01:13:15 -0700

    Added some animated caterpillars to the caterpillar room and scaled video

---
+++ Assets/VideoTriggerCat.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class VideoTriggerCat : MonoBehaviour
+{
+    public Canvas videoCanvascat;
+    public UnityEngine.Video.VideoPlayer videoPlayercat;
+
+    public UnityEngine.AudioSource audioPlayercat;
+
+
+    private void Start()
+    {
+        if (videoPlayercat != null)
+        {
+            videoPlayercat.Stop(); // Ensure video is reset
+            audioPlayercat.Stop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            videoCanvascat.enabled = true;
+
+            if (videoPlayercat != null)
+            {
+                videoPlayercat.Play();
+                audioPlayercat.Play();
+
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            videoCanvascat.enabled = false;
+
+            if (videoPlayercat != null)
+            {
+                videoPlayercat.Stop(); // Optional: or use .Pause() if you want to resume late
+                audioPlayercat.Stop();
+                videoPlayercat.time = 0; // Reset to start
+                audioPlayercat.time = 0;
+
+            }
+        }
+    }
+}
